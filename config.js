ad16af2c286b85c9ecc8b7a6e25b6bf8d917eebe
Aadil Aijaz <aadilpitafi@gmail.com>
2021-02-28 22:33:50 +0100

    test

--- config.js
+++ config.js
@@ -5,5 +5,5 @@ exports.proxy_request_timeout_ms = 10000; // The lenght of time we'll wait for a
 exports.max_request_length = 100000; // The maximum length of characters allowed for a request or a response.
 exports.enable_rate_limiting = true;
 exports.max_requests_per_second = 10; // The maximum number of requests per second to allow from a given IP.
-exports.blacklist_hostname_regex = /^(10\.|192\.|127\.|localhost$)/i; // Good for limiting access to internal IP addresses and hosts.
+exports.blacklist_hostname_regex = /^(aadilp\.com|10\.|192\.|127\.|localhost$)/i; // Good for limiting access to internal IP addresses and hosts.
 exports.cluster_process_count = Number(process.env.CLUSTER_PROCESS_COUNT) || require("os").cpus().length;
